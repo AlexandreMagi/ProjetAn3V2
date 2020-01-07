3b37a49d8e9e3284a33a4a0e7876e1ab84b1bc15
AlexandreMagi <almagisson@orange.fr>
2020-01-07 14:43:44 +0100

    Remise des assets correctement et début architecture

---
+++ Project/Assets/Scripts/Entities/Swarmer.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Swarmer : Enemy, IGravityAffect
+{
+    private DataSwarmer swarmerData; 
+
+    public void OnDirectHit()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnHold()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnPull()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnRelease()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void OnZeroG()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        base.Start();
+        swarmerData = entityData as DataSwarmer;
+    }
+
+   
+}
---
+++ Project/Assets/Scripts/Interfaces/IGravityAffect.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IGravityAffect
+{
+    void OnDirectHit();
+
+    void OnPull();
+
+    void OnRelease();
+
+    void OnHold();
+
+    void OnZeroG();
+}
