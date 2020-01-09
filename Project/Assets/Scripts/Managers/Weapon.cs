7023a5f3c3e0fde7639bcceec5986108dcce270d
SachaYerle <SachaYerle@users.noreply.github.com>
2020-01-09 16:04:47 +0100

    Fx modulaire

--- Project/Assets/Scripts/Managers/Weapon.cs
+++ Project/Assets/Scripts/Managers/Weapon.cs
@@ -89,7 +89,7 @@ public class Weapon : MonoBehaviour
                 RaycastHit hit;
                 if (Physics.Raycast(rayBullet, out hit, Mathf.Infinity, weapon.layerMaskHit))
                 {
-                    FxImpactDependingOnSurface(hit.transform.gameObject, hit.point);
+                    FxImpactDependingOnSurface(hit.transform.gameObject, hit.point, weaponMod);
                     CheckIfMustSlowMo(hit.transform.gameObject, weaponMod);
                     IBulletAffect bAffect = hit.transform.GetComponent<IBulletAffect>();
                     if (bAffect != null)
@@ -109,12 +109,20 @@ public class Weapon : MonoBehaviour
             TimeScaleManager.Instance.AddStopTime(weaponMod.bullet.timeStopAtImpact, 0, weaponMod.bullet.timeStopProbability);
     }
 
-    private void FxImpactDependingOnSurface(GameObject hit, Vector3 hitPoint)
+    private void FxImpactDependingOnSurface(GameObject hit, Vector3 hitPoint, DataWeaponMod weaponMod)
     {
+        for (int i = 0; i < weaponMod.bullet.bulletFxs.allFxReaction.Length; i++)
+        {
+            if (weaponMod.bullet.bulletFxs.allFxReaction[i].mask == (weaponMod.bullet.bulletFxs.allFxReaction[i].mask | (1 << hit.layer)))
+            {
+                FxManager.Instance.PlayFx(weaponMod.bullet.bulletFxs.allFxReaction[i].fxName, hitPoint, Quaternion.identity);
+            }
+        }
+/*
         if (hit.GetComponent<Enemy>() != null)
             FxManager.Instance.PlayFx("VFX_ImpactBlood", hitPoint, Quaternion.identity);
         else 
-            FxManager.Instance.PlayFx("VFX_ImpactWalls", hitPoint, Quaternion.identity);
+            FxManager.Instance.PlayFx("VFX_ImpactWalls", hitPoint, Quaternion.identity);*/
     }
 
     // Permet d'obtenir la valeur de charge pour les feedbacks -> renvoit le pourcentage de charge avec une marge de sécurité
