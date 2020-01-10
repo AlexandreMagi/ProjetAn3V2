3f6b3e3d7888b150ac474dd693c9671892065bf4
AlexandreMagi <almagisson@orange.fr>
2020-01-10 10:15:13 +0100

    Player

---
+++ Project/Assets/Scripts/Entities/Player.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : Entity
+{
+    public static Player Instance{get; private set;}
+
+    void Awake()
+    {
+        Instance = this;
+    }
+}
