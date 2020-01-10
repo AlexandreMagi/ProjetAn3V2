92a364fbdb7b2c9084e43f1f81e68a975c8f9244
AlexandreMagi <almagisson@orange.fr>
2020-01-10 09:43:11 +0100

    Début du swarmer follow + pathing

--- Project/Assets/Scripts/Entities/Swarmer.cs
+++ Project/Assets/Scripts/Entities/Swarmer.cs
@@ -3,13 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
-public class Swarmer : Enemy, IGravityAffect, IBulletAffect
+public class Swarmer : Enemy, IGravityAffect, IBulletAffect, IDetection
 {
-    private DataSwarmer swarmerData;
+    DataSwarmer swarmerData;
 
-    private bool isAirbone = false;
-    private float fTimePropel = .5f;
-    private float fElapsedTime = 0;
+    bool isAirbone = false;
+    float timePropel = .5f;
+    float elapsedTime = 0;
+    float timerWait = 0;
+
+    Pather pathToFollow;
+    int pathID = 0;
+
+    Transform currentFollow;
+    Vector3 v3VariancePoisitionFollow;
+
+    Rigidbody rbBody;
+
+    bool isChasingPlayer;
+    bool bIsDead = false;
+
+    enum State { Basic, Waiting, Attacking };
+    int nState = 0;
+
+    Transform target;
 
     //Stimulus
     #region Stimulus
@@ -78,35 +95,158 @@ public class Swarmer : Enemy, IGravityAffect, IBulletAffect
         throw new System.NotImplementedException();
     }
     #endregion
+    #region Detection
+    public void OnAudioDetect()
+    {
+        
+    }
+
+    public void OnSightDetect()
+    {
+        
+    }
+
+    public void OnDistanceDetect()
+    {
+        
+    }
+    #endregion
     #endregion
     // Start is called before the first frame update
     protected override void Start()
     {
         base.Start();
         swarmerData = entityData as DataSwarmer;
+        rbBody = GetComponent<Rigidbody>();
     }
 
-    void FixedUpdate()
+    public void SetPathToFollow(Pather path)
     {
+        pathToFollow = path;
 
+        currentFollow = path.GetPathAt(0);
+
+        v3VariancePoisitionFollow = currentFollow.position;
+    }
+
+    protected virtual void FixedUpdate()
+    {
         if (isAirbone)
         {
-            fElapsedTime += Time.deltaTime;
+            elapsedTime += Time.deltaTime;
 
-            if (fElapsedTime >= fTimePropel)
+            if (elapsedTime >= timePropel)
             {
                 ReactGravity.DoSpin(this);
 
                 //Check si touche le sol
-                fElapsedTime = 0;
+                elapsedTime = 0;
                 if (Physics.Raycast(this.transform.position, new Vector3(0, -1, 0), 1f))
                 {
                     isAirbone = false;
                 }
+
             }
+
         }
 
+        if (pathToFollow != null && currentFollow != null && swarmerData != null && rbBody.useGravity && !isAirbone)
+        {
+            if (nState == (int)State.Basic)
+            {
+                if (isChasingPlayer)
+                {
+                    v3VariancePoisitionFollow = target.position;
+
+                }
+
+                //TODO : Follow the path
+                Vector3 direction = (new Vector3(v3VariancePoisitionFollow.x, transform.position.y, v3VariancePoisitionFollow.z) - transform.position).normalized;
+
+                rbBody.AddForce(direction * swarmerData.speed + Vector3.up * Time.fixedDeltaTime * swarmerData.upScale);
+
+                //transform.Translate(direction * swarmer.speed * Time.deltaTime);
+
+                if (!isChasingPlayer)
+                {
+                    if (Vector2.Distance(new Vector2(transform.position.x, transform.position.z), new Vector2(v3VariancePoisitionFollow.x, v3VariancePoisitionFollow.z)) < swarmerData.fDistanceBeforeNextPath)
+                    {
+                        currentFollow = pathToFollow.GetPathAt(pathID++);
+                        if (currentFollow == null) pathID--;
+
+                        if (currentFollow != null && currentFollow != target)
+                        {
+                            //Debug.Log("Proc variance, variance = "+swarmer.nVarianceInPath+"%");
+                            //Debug.Log("Variance = "+ (swarmer.nVarianceInPath / 100 * Random.Range(-2f, 2f)));
+
+                            v3VariancePoisitionFollow = new Vector3(
+                                currentFollow.position.x + (swarmerData.nVarianceInPath / 100 * Random.Range(-2f, 2f)),
+                                currentFollow.position.y,
+                                currentFollow.position.z + (swarmerData.nVarianceInPath / 100 * Random.Range(-2f, 2f))
+                            );
+
+                            //Debug.Log("Initial pos X: " + currentFollow.position.x + " - Varied pos X : " + v3VariancePoisitionFollow.x);
+                        }
+                        else
+                        {
+                            currentFollow = target;
+                        }
+
+                    }
+
+                    if (Vector2.Distance(new Vector2(transform.position.x, transform.position.z), new Vector2(target.position.x, target.position.z)) < swarmerData.distanceToTargetPlayer)
+                    {
+                        isChasingPlayer = true;
+                        currentFollow = target;
+                    }
+                }
+                if (CheckDistance() && Physics.Raycast(this.transform.position, new Vector3(0, -1, 0), 0.5f) && transform.position.y < target.position.y + 1)
+                {
+                    nState = (int)State.Waiting;
+                    rbBody.velocity = Vector3.zero;
+                    GetComponent<Animator>().SetTrigger("PrepareToJump");
+                }
+            }
+            else if (nState == (int)State.Waiting)
+            {
+                timerWait += Time.deltaTime;
+                if (timerWait > swarmerData.fWaitDuration)
+                {
+                    timerWait = 0;
+                    if (CheckDistance())
+                    {
+                        nState = (int)State.Attacking;
+                        GetComponentInChildren<MeshRenderer>().material.SetColor("_BaseColor", Color.red);
+                        GetComponentInChildren<MeshRenderer>().material.SetColor("_EmissionColor", Color.red);
+                        rbBody.AddForce(Vector3.up * swarmerData.fJumpForce, ForceMode.Impulse);
+                        CustomSoundManager.Instance.PlaySound(Camera.main.gameObject, "SE_Swarmer_Attack", false, 0.4f, 0.3f);
+                    }
+                    else
+                        nState = (int)State.Basic;
+
+                }
+            }
+            else if (nState == (int)State.Attacking)
+            {
+                //TODO : Follow the path
+                Vector3 direction = (new Vector3(target.position.x, transform.position.y, target.position.z) - transform.position).normalized;
+                rbBody.AddForce(direction * swarmerData.speed * swarmerData.fSpeedMultiplierWhenAttacking + Vector3.up * Time.fixedDeltaTime * swarmerData.upScale);
+                if (!CheckDistance())
+                {
+                    nState = (int)State.Basic;
+                    GetComponentInChildren<MeshRenderer>().material.SetColor("_BaseColor", Color.Lerp(Color.yellow, Color.red, 0.5f));
+                    GetComponentInChildren<MeshRenderer>().material.SetColor("_EmissionColor", Color.Lerp(Color.yellow, Color.red, 0.5f));
+                }
+            }
+        }
     }
 
+    bool CheckDistance()
+    {
+        if (Vector3.Distance(transform.position, target.position) < swarmerData.fDistanceBeforeAttack)
+            return true;
+        else
+            return false;
+    }
 
 }
---
+++ Project/Assets/Scripts/Interfaces/IDetection.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDetection
+{
+    void OnAudioDetect();
+
+    void OnSightDetect();
+
+    void OnDistanceDetect();
+}
