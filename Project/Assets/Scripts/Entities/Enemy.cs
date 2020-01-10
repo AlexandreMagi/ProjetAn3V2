9c8120c42e3016261be5451f96f52a54483d38d7
AlexandreMagi <almagisson@orange.fr>
2020-01-10 09:47:45 +0100

    Update Enemy

--- Project/Assets/Scripts/Entities/Enemy.cs
+++ Project/Assets/Scripts/Entities/Enemy.cs
@@ -9,6 +9,8 @@ public class Enemy : Entity
     bool isStun = false;
     private DataEnemy enemyData;
 
+    protected Transform target;
+
     // Start is called before the first frame update
     protected override void Start()
     {
