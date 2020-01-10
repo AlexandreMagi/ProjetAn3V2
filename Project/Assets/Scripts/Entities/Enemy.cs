9393ee4c56492094d2d947ebe3dee91d46c6b189
AlexandreMagi <almagisson@orange.fr>
2020-01-10 12:11:24 +0100

    Merge

--- Project/Assets/Scripts/Entities/Enemy.cs
+++ Project/Assets/Scripts/Entities/Enemy.cs
@@ -50,4 +50,36 @@ public class Enemy : Entity
         }
     }
 
+    IEnumerator CheckForTargets()
+    {
+        while (true)
+        {
+            yield return new WaitForSecondsRealtime(.15f);
+
+            //Recherche de cible à attaquer
+            enemies = TeamsManager.Instance.GetAllEnemiesFromTeam(this.enemyData.team);
+            if (enemies.Count > 0)
+            {
+                distanceToClosest = Vector2.Distance(new Vector2(transform.position.x, transform.position.z), new Vector2(enemies[0].position.x, enemies[0].position.z));
+                possibleTarget = enemies[0];
+
+                if (enemies.Count > 1)
+                {
+                    for (int i = 1; i < enemies.Count; i++)
+                    {
+                        float distanceTemp = Vector2.Distance(new Vector2(transform.position.x, transform.position.z), new Vector2(enemies[i].position.x, enemies[i].position.z));
+                        if (distanceTemp < distanceToClosest)
+                        {
+                            distanceToClosest = distanceTemp;
+                            possibleTarget = enemies[i];
+                        }
+                    }
+                }
+
+                OnDistanceDetect(possibleTarget, distanceToClosest);
+            }
+        }
+
+    }
+
 }
