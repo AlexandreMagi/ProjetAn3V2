cf4526ddcf067d0ae3afc9113c774d420a27fd29
AlexandreMagi <almagisson@orange.fr>
2020-01-20 10:16:46 +0100

    Y'a le bouton magique mais il fonctionne pas

--- Project/Assets/Scripts/Managers/PublicManager.cs
+++ Project/Assets/Scripts/Managers/PublicManager.cs
@@ -6,11 +6,14 @@ public class PublicManager : MonoBehaviour
 {
     uint nbViewers = 0;
 
+    List<ActionType> stallBuffer;
+
     float currentMultiplier = 1;
 
     void Awake()
     {
         Instance = this;
+        stallBuffer = new List<ActionType>();
     }
 
     public static PublicManager Instance { get; private set; }
@@ -18,5 +21,31 @@ public class PublicManager : MonoBehaviour
     public uint GetNbViewers()
     {
         return nbViewers;
+    }
+
+    public void OnEpicAction(string actionName)
+    {
+        switch (actionName)
+        {
+
+        }
+    }
+
+    public void OnLameAction(string actionName)
+    {
+
+    }
+
+    public enum ActionType
+    {
+        EnvironmentKill = 0,
+        RefuseBonus = 1,
+        PerfectProjectile = 2,
+        BackToSender = 3,
+        Kill = 4,
+        PerfectReload = 5,
+        Vendetta = 6,
+        SuperLowHp = 7,
+        LowHp = 8,
     }
 }
