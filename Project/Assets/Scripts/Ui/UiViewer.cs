88682a4b98eba09f53bd0e162603abfaf8fb8edf
SachaYerle <SachaYerle@users.noreply.github.com>
2020-01-20 11:15:13 +0100

    Ui public

---
+++ Project/Assets/Scripts/Ui/UiViewer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UiViewer : MonoBehaviour
+{
+
+    void Awake()
+    {
+        Instance = this;
+    }
+    public static UiViewer Instance { get; private set; }
+
+    [SerializeField] Text ViewerText = null;
+    float currentViewerFluid = 0;
+    [SerializeField] float viewerSpeedDisplay = 5;
+    [SerializeField] float considerDifference = 50;
+    [SerializeField] GameObject upArrow = null;
+    [SerializeField] GameObject downArrow = null;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (PublicManager.Instance != null)
+        {
+            uint actualViewer = PublicManager.Instance.GetNbViewers();
+            currentViewerFluid = Mathf.Lerp(currentViewerFluid, actualViewer, Time.deltaTime * viewerSpeedDisplay);
+
+            if (Mathf.Abs(currentViewerFluid - actualViewer) > considerDifference)
+            {
+                if (currentViewerFluid > actualViewer)
+                {
+                    // Go Up
+                    upArrow.SetActive(true);
+                    downArrow.SetActive(false);
+                }
+                else
+                {
+                    // Go Down
+                    upArrow.SetActive(false);
+                    downArrow.SetActive(true);
+                }
+            }
+            else
+            {
+                // Is Stable
+                upArrow.SetActive(false);
+                downArrow.SetActive(false);
+            }
+
+            ViewerText.text = $"{Mathf.RoundToInt(currentViewerFluid)}";
+        }
+        else ViewerText.text = "NO MANAGER";
+    }
+}
