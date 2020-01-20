82ea7d033665bf0b89a2134861aec66455ceb72f
AlexandreMagi <almagisson@orange.fr>
2020-01-20 09:11:30 +0100

    Début public manager

---
+++ Project/Assets/Scripts/Managers/PublicManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PublicManager : MonoBehaviour
+{
+    uint nbViewers = 0;
+
+    float currentMultiplier = 1;
+
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    public static PublicManager Instance { get; private set; }
+
+    public uint GetNbViewers()
+    {
+        return nbViewers;
+    }
+}
