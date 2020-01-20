d1a50cb20a9574bda12950bcdea833f1a8ddd75c
AlexandreMagi <almagisson@orange.fr>
2020-01-20 11:06:36 +0100

    Rebuild public manager

--- Project/Assets/Scripts/Managers/PublicManager.cs
+++ Project/Assets/Scripts/Managers/PublicManager.cs
@@ -4,9 +4,16 @@ using UnityEngine;
 
 public class PublicManager : MonoBehaviour
 {
-    uint nbViewers = 0;
+    int nbViewers = 0;
 
     List<ActionType> stallBuffer;
+
+    [SerializeField]
+    int bufferSize = 8;
+
+    int baseViewerGrowth = 50;
+    int randomViewerGrowth = 8;
+    float bufferStallAffect = .8f;
 
     float currentMultiplier = 1;
 
@@ -18,22 +25,69 @@ public class PublicManager : MonoBehaviour
 
     public static PublicManager Instance { get; private set; }
 
-    public uint GetNbViewers()
+    public int GetNbViewers()
     {
         return nbViewers;
     }
 
-    public void OnEpicAction(string actionName)
+    public void OnPlayerAction(ActionType action)
     {
-        switch (actionName)
+        switch (action)
         {
-
+            case ActionType.EnvironmentKill:
+                AddViewers(4, true, action);
+                AddToBuffer(action);
+                break;
+            case ActionType.RefuseBonus:
+                break;
+            case ActionType.PerfectProjectile:
+                break;
+            case ActionType.BackToSender:
+                break;
+            case ActionType.Kill:
+                break;
+            case ActionType.PerfectReload:
+                break;
+            case ActionType.Vendetta:
+                break;
+            case ActionType.SuperLowHp:
+                break;
+            case ActionType.LowHp:
+                break;
+            case ActionType.DamageOnLifeBar:
+                break;
+            case ActionType.Repeat:
+                break;
+            case ActionType.MissGravityOrb:
+                break;
+            case ActionType.MissShotGun:
+                break;
+            case ActionType.DeathAndRespawn:
+                break;
+            default:
+                break;
         }
     }
 
-    public void OnLameAction(string actionName)
+    private void AddViewers(int viewerLevel, bool isAffectedByBuffer, ActionType action)
     {
+        float bufferMultiplier = 1;
+        if (isAffectedByBuffer)
+        {
+            foreach (ActionType actionInTab in stallBuffer)
+            {
+                if (actionInTab == action) bufferMultiplier *= bufferStallAffect;
+            }
+        }
+       
 
+        nbViewers += Mathf.FloorToInt((baseViewerGrowth + Random.Range(0, randomViewerGrowth)) * viewerLevel * bufferStallAffect);
+    }
+
+    private void AddToBuffer(ActionType action)
+    {
+        stallBuffer.Add(action);
+        if (stallBuffer.Count > bufferSize) stallBuffer.RemoveAt(0);
     }
 
     public enum ActionType
@@ -47,5 +101,10 @@ public class PublicManager : MonoBehaviour
         Vendetta = 6,
         SuperLowHp = 7,
         LowHp = 8,
+        DamageOnLifeBar = 9,
+        Repeat = 10,
+        MissGravityOrb = 11,
+        MissShotGun = 12,
+        DeathAndRespawn = 13,
     }
 }
