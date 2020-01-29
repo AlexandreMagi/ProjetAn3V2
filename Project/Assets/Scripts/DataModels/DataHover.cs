723f72d7a3257f185598fee4ec965735f9535be8
AlexandreMagi <almagisson@orange.fr>
2020-01-29 15:59:34 +0100

    Hover begin

--- Project/Assets/Scripts/DataModels/DataHover.cs
+++ Project/Assets/Scripts/DataModels/DataHover.cs
@@ -5,5 +5,9 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Enemies/DataHover")]
 public class DataHover : DataEnemy
 {
-    
+    public float shieldApplyRange;
+
+    public float shieldRadius;
+
+    public float timeShieldCast;
 }
