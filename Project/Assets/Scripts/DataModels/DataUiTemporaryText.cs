e9dab39cfa2799f8d7d2f32a7a8697ec03bf89dc
SachaYerle <SachaYerle@users.noreply.github.com>
2020-02-04 16:45:47 +0100

    Score placé dans l'ui

---
+++ Project/Assets/Scripts/DataModels/DataUiTemporaryText.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using Sirenix.OdinInspector;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/DataUiTemporaryText")]
+public class DataUiTemporaryText : ScriptableObject
+{
+    public Color colorMain = Color.red;
+    public Color colorOutline = Color.black;
+
+    public float fontSize = 100;
+
+    public float timeToScaleToMax = 0.5f;
+    public float timeStayVisible = 1;
+    public float timeToFade = 1;
+
+    public float outlineDistance = 5;
+
+    public float randomPos = 0.4f;
+}
+
+
---
+++ Project/Assets/Scripts/Ui/ScoreBonusDisplayedInstance.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreBonusDisplayedInstance
+{
+    public Color currentColor = Color.red;
+    private DataUiTemporaryText data = null;
+    private float currentTimer = 0;
+    public float scale = 0;
+    private float currentAlpha = 1;
+    public bool isPlacedOnWorld = false;
+    public Vector3 posSave = Vector3.zero;
+
+    public void OnCreation(DataUiTemporaryText _data)
+    {
+        data = _data;
+        currentColor = _data.colorMain;
+    }
+
+    public void IsPlacedInWorld(bool _isPlacedOnWorld, Vector3 _posSave)
+    {
+        isPlacedOnWorld = _isPlacedOnWorld;
+        posSave = _posSave;
+    }
+
+    public void UpdateValues()
+    {
+        scale = Mathf.MoveTowards(scale, 1, Time.unscaledDeltaTime / data.timeToScaleToMax);
+
+        currentTimer += Time.unscaledDeltaTime;
+        if (currentTimer > data.timeStayVisible)
+        {
+            currentAlpha -= Time.unscaledDeltaTime / data.timeToFade;
+            if (currentAlpha < 0)
+                UiScoreBonusDisplay.Instance.deleteSpot(this);
+            else
+                currentColor = new Color(data.colorMain.r, data.colorMain.g, data.colorMain.b, currentAlpha);
+        }
+    }
+
+}
