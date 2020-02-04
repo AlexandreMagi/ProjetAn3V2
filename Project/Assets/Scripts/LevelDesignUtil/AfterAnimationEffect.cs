f371aec4c2aaf379a05d30af544a64acaff4c8b7
AnlaXix <wirtz.maxime1@gmail.com>
2020-02-04 11:25:12 +0100

    LD_03 reste plus que les shooter à gérer

---
+++ Project/Assets/Scripts/LevelDesignUtil/AfterAnimationEffect.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AfterAnimationEffect : MonoBehaviour
+{
+
+    [SerializeField]
+    GameObject[] objectToDisable = null;
+
+    [SerializeField]
+    GameObject[] objectToEnable = null;
+
+    void disableGameobject()
+    {
+        if (objectToDisable != null)
+            foreach (GameObject obj in objectToDisable)
+            {
+                obj.SetActive(false);
+            }
+    }
+    void enableGameobject()
+    {
+
+        if (objectToEnable != null)
+            foreach (GameObject obj in objectToEnable)
+            {
+                obj.SetActive(true);
+            }
+    }
+}
