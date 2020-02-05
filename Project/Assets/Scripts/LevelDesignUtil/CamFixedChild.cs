aa93e4998d29b951bd5d17e37433d64e2473aece
SachaYerle <SachaYerle@users.noreply.github.com>
2020-02-05 11:32:14 +0100

    Ya D Kméra

---
+++ Project/Assets/Scripts/LevelDesignUtil/CamFixedChild.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFixedChild : MonoBehaviour, IBulletAffect
+{
+
+    public FixedCameraScript parentScript;
+
+    #region bulletAffect
+    public void OnBulletClose() { }
+
+    public void OnHit(DataWeaponMod mod, Vector3 position)
+    {
+        parentScript.hitByBullet();
+    }
+
+    public void OnHitShotGun() { }
+
+    public void OnHitSingleShot() { }
+    #endregion
+}
---
+++ Project/Assets/Scripts/LevelDesignUtil/FixedCameraScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FixedCameraScript : MonoBehaviour
+{
+
+    [SerializeField]
+    Transform lookAtTarget = null;
+    [SerializeField]
+    Transform cameraDummy = null;
+    [SerializeField]
+    bool activatedCam = true;
+    [SerializeField]
+    float camSpeed = 5;
+    [SerializeField]
+    float exploSize = 1;
+    [SerializeField]
+    Vector2 camShake = new Vector2(0.3f, 0.4f);
+    [SerializeField]
+    string fxName = "VFX_ExplosionShooterBullet";
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (lookAtTarget == null) lookAtTarget = Player.Instance.transform;
+        cameraDummy.GetComponent<CamFixedChild>().parentScript = this;
+    }
+
+    public void hitByBullet()
+    {
+        FxManager.Instance.PlayFx(fxName, cameraDummy.position, cameraDummy.rotation, exploSize);
+        CameraHandler.Instance.AddShake(camShake.x, camShake.y);
+        Destroy(this.gameObject);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Quaternion currentRot = cameraDummy.rotation;
+        Quaternion newRot;
+
+        if (activatedCam) cameraDummy.LookAt(lookAtTarget, Vector3.up);
+        else cameraDummy.LookAt(cameraDummy.position + Vector3.down, Vector3.up);
+        newRot = cameraDummy.rotation;
+        cameraDummy.rotation = Quaternion.Lerp(currentRot, newRot, Time.deltaTime * camSpeed);
+    }
+}
