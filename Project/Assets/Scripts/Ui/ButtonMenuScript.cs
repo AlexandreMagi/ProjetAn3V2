25a01fc504039817064c99e9057e128126296acd
SachaYerle <SachaYerle@users.noreply.github.com>
2020-02-07 12:39:18 +0100

    Menu début

---
+++ Project/Assets/Scripts/Ui/ButtonMenuScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+public class ButtonMenuScript : MonoBehaviour
+{
+    RectTransform rect;
+
+    private void Start()
+    {
+        rect = GetComponent<RectTransform>();
+        if (MenuMain.Instance != null) MenuMain.Instance.buttonMenuScripts.Add(this);
+    }
+
+    public bool CheckIfMouseOver(Vector2 mousePosition)
+    {
+        float distX = rect.sizeDelta.x / 2 * transform.localScale.x;
+        float distY = rect.sizeDelta.y / 2 * transform.localScale.y;
+        if (mousePosition.x < rect.position.x + distX && mousePosition.x > rect.position.x - distX && mousePosition.y < rect.position.y + distY && mousePosition.y > rect.position.y - distY) 
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    public void Click(Vector2 mousePosition)
+    {
+        if (CheckIfMouseOver(mousePosition))
+        {
+            transform.localScale = Vector3.zero;
+        }
+    }
+
+}
---
+++ Project/Assets/Scripts/Ui/MenuMain.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuMain : MonoBehaviour
+{
+
+    private static MenuMain _instance;
+    public static MenuMain Instance { get { return _instance; } }
+    void Awake() { _instance = this; }
+
+    public List<ButtonMenuScript> buttonMenuScripts;
+
+    enum menustate { intro, home, mainmenu }
+    menustate currentState = menustate.intro;
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        switch (currentState)
+        {
+            case menustate.intro:
+                if (Input.GetKeyDown(KeyCode.Mouse0)) SkipToHome();
+                break;
+            case menustate.home:
+                break;
+            case menustate.mainmenu:
+                foreach (var button in buttonMenuScripts)
+                {
+                    bool mouseOver = button.CheckIfMouseOver(Input.mousePosition);
+                    if (mouseOver)
+                    {
+                        button.transform.localScale = Vector3.Lerp(button.transform.localScale, Vector3.one * 1.2f, Time.unscaledDeltaTime);
+                    }
+                    else
+                    {
+                        button.transform.localScale = Vector3.Lerp(button.transform.localScale, Vector3.one * 1.0f, Time.unscaledDeltaTime);
+                    }
+                }
+                if (Input.GetKeyDown(KeyCode.Mouse0)) Click(Input.mousePosition);
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    void Click(Vector2 mousePosition)
+    {
+        foreach (var button in buttonMenuScripts)
+        {
+            button.Click(mousePosition);
+        }
+    }
+
+    void SkipToHome()
+    {
+    }
+
+    public void IsOnMainMenu()
+    {
+        currentState = menustate.mainmenu;
+    }
+
+    public void IsOnHome()
+    {
+        currentState = menustate.home;
+    }
+
+    public void IsOnIntro()
+    {
+        currentState = menustate.intro;
+    }
+
+}
