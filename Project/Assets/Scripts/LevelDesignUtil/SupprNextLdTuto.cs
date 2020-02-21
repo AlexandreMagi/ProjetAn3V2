3008d476f7b6c828476e6016f0196b0ced9fc516
AnlaXix <wirtz.maxime1@gmail.com>
2020-02-21 15:50:11 +0100

    WIP Hint grid

---
+++ Project/Assets/Scripts/LevelDesignUtil/SupprNextLdTuto.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SupprNextLdTuto : MonoBehaviour, IBulletAffect
+{
+    [SerializeField]
+    int baseShootCountBeforeFirstInt = 5;
+    [SerializeField]
+    int baseShootCountBeforeSecondInt = 15;
+
+    int baseShootCountIncrement;
+
+    public void OnBulletClose()
+    {
+    }
+
+    public void OnHit(DataWeaponMod mod, Vector3 position)
+    {
+    }
+
+    public void OnHitShotGun(DataWeaponMod mod)
+    {
+    }
+
+    public void OnHitSingleShot(DataWeaponMod mod)
+    {
+        Debug.Log("BasicHit");
+
+        baseShootCountIncrement++;
+
+        if (baseShootCountIncrement == baseShootCountBeforeFirstInt)
+        {
+            Debug.Log("First hint");
+        }
+        else if (baseShootCountIncrement == baseShootCountBeforeSecondInt)
+        {
+            Debug.Log("Second hint");
+        }
+    }
+}
