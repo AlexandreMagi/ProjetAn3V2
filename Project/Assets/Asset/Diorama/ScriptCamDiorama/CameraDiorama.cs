33dada375061f290715f40d425eb144ccf7eb191
Sakapatate <ferezparvin.quentin@gmail.com>
2020-02-24 15:24:14 +0100

    MajSceneDiorama
    Grosse maj mamene

---
+++ Project/Assets/Asset/Diorama/ScriptCamDiorama/CameraDiorama.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraDiorama : MonoBehaviour
+{
+
+
+    [SerializeField]
+    GameObject hCam;
+
+    [SerializeField]
+    [Tooltip("en seconde pour un tour")]
+    float floatVitesseRotation;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        transform.Rotate(new Vector3(0,360/ floatVitesseRotation, 0)*Time.deltaTime,Space.Self);
+
+    }
+}
