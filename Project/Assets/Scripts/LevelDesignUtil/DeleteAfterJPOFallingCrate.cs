3d51e423ca33a8fe7fc21649c73c77df8654d3e6
AnlaXix <wirtz.maxime1@gmail.com>
2020-02-25 13:15:01 +0100

    On peut finir le jeu

---
+++ Project/Assets/Scripts/LevelDesignUtil/DeleteAfterJPOFallingCrate.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteAfterJPOFallingCrate : MonoBehaviour
+{
+    [SerializeField]
+    GameObject trigger = null;
+
+    CapsuleCollider col;
+
+    Rigidbody rb;
+
+    bool safe = false;
+
+    private void Start()
+    {
+        col = trigger.GetComponent<CapsuleCollider>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (col.enabled == false && !safe)
+        {
+            rb.isKinematic = false;
+            safe = true;
+        }
+    }
+}
