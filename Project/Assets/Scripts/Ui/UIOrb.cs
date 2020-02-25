a93637ec2d0c4fe7031c5a9121fcd5728ee73fad
SachaYerle <SachaYerle@users.noreply.github.com>
2020-02-25 13:42:12 +0100

    UI avec orb

---
+++ Project/Assets/Scripts/Ui/UIOrb.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIOrb : MonoBehaviour
+{
+
+    [SerializeField] ScriptIdleASuprimerPostJPO[] orbs = new ScriptIdleASuprimerPostJPO[0];
+
+    // Update is called once per frame
+    void Update()
+    {
+        float currVal = Weapon.Instance.GetOrbValue();
+        foreach (var orb in orbs)
+        {
+            orb.refScale = currVal > 1 ? 1: currVal;
+            orb.scaleIdle = currVal > 1;
+        }
+    }
+}
