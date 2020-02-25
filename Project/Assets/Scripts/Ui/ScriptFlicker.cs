610c567367afeda8d1c39d6947d928ca1c2267c8
SachaYerle <SachaYerle@users.noreply.github.com>
2020-02-25 14:41:01 +0100

    Ui anim

---
+++ Project/Assets/Scripts/Ui/ScriptFlicker.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class ScriptFlicker : MonoBehaviour
+{
+
+    [SerializeField] bool independantFromTimeScale = false;
+    [SerializeField] bool flickerPosition = false;
+    [SerializeField, ShowIf("flickerPosition")] float moveRange = 0.1f;
+    [SerializeField, ShowIf("flickerPosition")] float moveFlickDuration = 0.1f;
+    Vector3 initPos;
+    Vector3 currentPos;
+    Vector3 targetPos = Vector3.zero;
+    float timeLeftPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initPos = transform.localPosition;// Save de la position de base
+        currentPos = initPos; // Setup de la var de position à celle de base
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (flickerPosition)
+        {
+            float dt = independantFromTimeScale ? Time.unscaledDeltaTime : Time.deltaTime;
+            if (timeLeftPos < dt)
+            {
+                targetPos = initPos + Random.insideUnitSphere * moveRange;
+                timeLeftPos = moveFlickDuration;
+            }
+            else
+            {
+                float valueLerp = dt / timeLeftPos; // "poids" du changement en fonction du dt, on divise par le temps restant pour que le deplacement soit fluide
+                currentPos = Vector3.Lerp(currentPos, targetPos, valueLerp); // lerp de la valeur actuelle vers la valeur visée
+                transform.localPosition = currentPos;
+                timeLeftPos -= dt;
+            }
+
+        }
+    }
+}
--- Project/Assets/Scripts/Ui/UINewWait.cs
+++ Project/Assets/Scripts/Ui/UINewWait.cs
@@ -28,13 +28,15 @@ public class UINewWait : MonoBehaviour
             obj.SetActive(false);
         }
         waitRoot.SetActive(true);
-        waitAnmatr.SetTrigger("pop");
+        if (waitRoot.activeSelf)
+            waitAnmatr.SetTrigger("pop");
         
     }
 
     public void RemoveWait()
     {
-        waitAnmatr.SetTrigger("depop");
+        if (waitRoot.activeSelf)
+            waitAnmatr.SetTrigger("depop");
         Invoke("RemoveWaitTrue", 0.39f);
     }
 
