70f0137fe73d8e36c6c53a3aedb0dd54a6f76e32
SachaYerle <SachaYerle@users.noreply.github.com>
2020-02-26 18:01:38 +0100

    ufytfj

--- Project/Assets/Scripts/Ui/UIOrb.cs
+++ Project/Assets/Scripts/Ui/UIOrb.cs
@@ -4,12 +4,31 @@ using UnityEngine;
 
 public class UIOrb : MonoBehaviour
 {
+    private static UIOrb _instance;
+    public static UIOrb Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+    void Awake()
+    {
+        _instance = this;
+    }
 
     [SerializeField] ScriptIdleASuprimerPostJPO[] orbs = new ScriptIdleASuprimerPostJPO[0];
     [SerializeField] AnimationCurve animWhenFull = AnimationCurve.Linear(0,0,1,1);
     [SerializeField] float animMultiplier = 0.3f;
     [SerializeField] float animTime = 0.8f;
     float animPurcentage = 0;
+
+
+    [SerializeField] GameObject orbObtainedMesh = null;
+    [SerializeField] AnimationCurve animWhenObtained = AnimationCurve.Linear(0, 0, 1, 1);
+    [SerializeField] float animMultiplierObtained = 0.3f;
+    [SerializeField] float animTimeObtained = 0.8f;
+    float animObtained = 1;
 
     // Update is called once per frame
     void Update()
@@ -27,11 +46,26 @@ public class UIOrb : MonoBehaviour
         {
             animPurcentage = 0;
         }
-
+        /*
+        if (animObtained < 1)
+        {
+            animObtained += Time.unscaledDeltaTime / animTimeObtained;
+        }
+        if (animObtained > 1)
+        {
+            orbObtainedMesh.transform.localScale = Vector3.zero;
+            animObtained = 1;
+        }
+        */
         foreach (var orb in orbs)
         {
             orb.refScale = currVal > 1 ? 1 + animWhenFull.Evaluate(animPurcentage) * animMultiplier : currVal;
             orb.scaleIdle = currVal > 1;
         }
+    }
+
+    public void ActivateOrb()
+    {
+
     }
 }
