a914de9734197eb9f3a3c522538a80456b0d5e2e
Sakapatate <ferezparvin.quentin@gmail.com>
2020-02-26 16:38:59 +0100

    Grosse maj encore

---
+++ Project/Assets/Asset/Diorama/SwarmerAnim/aiSwarmerAnim.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class aiSwarmerAnim : MonoBehaviour
+{
+
+    private Animator Swanimator;
+    private int SwanIdle = Animator.StringToHash("Idle");
+
+    [SerializeField] private string[] allTrigger = new string[0];
+    private int[] allTriggerHash = new int[0];
+
+    private int OtherAnim = Animator.StringToHash("Intimidation");
+    private float TimeLeft;
+
+    void Start()
+    {
+        Swanimator = GetComponent<Animator>();
+        TimeLeft = Random.Range(1.0f, 3.0f);
+
+        allTriggerHash = new int[allTrigger.Length];
+        for (int i = 0; i < allTrigger.Length; i++)
+        {
+            allTriggerHash[i] = Animator.StringToHash(allTrigger[i]);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (TimeLeft < Time.deltaTime)
+        {
+            Swanimator.SetTrigger(allTriggerHash[Random.Range(0, allTriggerHash.Length)]);
+            TimeLeft += Random.Range(3f, 8f);
+        }
+        else
+        {
+            TimeLeft -= Time.deltaTime;
+        }
+    }
+}
