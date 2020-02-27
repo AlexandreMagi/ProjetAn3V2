219ea46df1f5caa0217603812aa3d6e5dd104277
AnlaXix <wirtz.maxime1@gmail.com>
2020-02-27 14:57:33 +0100

    Plein de son

---
+++ Project/Assets/Scripts/LevelDesignUtil/AudioSourceManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSourceManager : MonoBehaviour
+{
+
+    public void SetAudioSource(bool isActiveOnTrigger)
+    {
+        if (isActiveOnTrigger)
+        {
+            AudioSource[] aud = GetComponents<AudioSource>();
+            foreach (AudioSource a in aud)
+            {
+                a.enabled = true;
+            }
+        }else if (!isActiveOnTrigger)
+        {
+            AudioSource[] aud = GetComponents<AudioSource>();
+            foreach (AudioSource a in aud)
+            {
+                a.enabled = false;
+            }
+        }
+    }
+
+}
