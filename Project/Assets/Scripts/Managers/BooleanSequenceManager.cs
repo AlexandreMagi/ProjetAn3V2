12c087b25337cd3532fcc3c9eddbc052f1e84e60
AlexandreMagi <almagisson@orange.fr>
2020-02-27 13:36:08 +0100

    Le bug relou

--- Project/Assets/Scripts/Managers/BooleanSequenceManager.cs
+++ Project/Assets/Scripts/Managers/BooleanSequenceManager.cs
@@ -48,30 +48,3 @@ public class BooleanSequenceManager : MonoBehaviour
         return false;
     }
 }
-
-[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/Sequences/DataBooleanSequence")]
-public class DataBooleanSequence : ScriptableObject
-{
-    [SerializeField]
-    string boolName = "";
-
-    [SerializeField]
-    bool defaultState = false;
-
-    public BooleanSequence OnInit()
-    {
-        return new BooleanSequence(boolName, defaultState);
-    }
-}
-
-public class BooleanSequence
-{
-    public string boolName = "";
-    public bool runtimeState = false;
-
-    public BooleanSequence(string name, bool stateInit)
-    {
-        boolName = name;
-        runtimeState = stateInit;
-    }
-}
