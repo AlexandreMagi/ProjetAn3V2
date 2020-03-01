b3db66d3d28faaa34db254d3593541f253fcec0e
AnlaXixDejaPris <59562425+AnlaXixDejaPris@users.noreply.github.com>
2020-03-01 20:39:02 +0100

    Début destruction environnemental

---
+++ Project/Assets/Scripts/VFX/TempoWallBreak.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TempoWallBreak : MonoBehaviour, IBulletAffect
+{
+
+    MeshRenderer renderer;
+    Rigidbody rb;
+
+    void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    public void OnBulletClose()
+    {
+    }
+
+    public void OnHit(DataWeaponMod mod, Vector3 position, float dammage)
+    {
+        renderer.enabled = true;
+        rb.isKinematic = false;
+        rb.AddForce(-position.normalized*100);
+    }
+
+    public void OnHitShotGun(DataWeaponMod mod)
+    {
+    }
+
+    public void OnHitSingleShot(DataWeaponMod mod)
+    {
+    }
+}
