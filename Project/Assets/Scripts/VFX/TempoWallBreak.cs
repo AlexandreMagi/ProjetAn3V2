278c38d9c518fb5b88d88137bc97f91c3f30f470
AnlaXixDejaPris <59562425+AnlaXixDejaPris@users.noreply.github.com>
2020-03-02 03:53:56 +0100

    Destrucion wall un peut plus avancée

--- Project/Assets/Scripts/VFX/TempoWallBreak.cs
+++ Project/Assets/Scripts/VFX/TempoWallBreak.cs
@@ -4,14 +4,63 @@ using UnityEngine;
 
 public class TempoWallBreak : MonoBehaviour, IBulletAffect
 {
-
-    MeshRenderer renderer;
     Rigidbody rb;
+    SphereCollider shCollider;
+    MeshCollider meCollider;
+
+    bool canBeAffectedByGravity = true;
 
     void Start()
     {
-        renderer = GetComponent<MeshRenderer>();
         rb = GetComponent<Rigidbody>();
+        shCollider = GetComponent<SphereCollider>();
+        meCollider = GetComponent<MeshCollider>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.layer == 9 && canBeAffectedByGravity)
+        {
+            if (rb.isKinematic != false)
+                rb.isKinematic = false;
+        }
+    }
+
+    void setStatic()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.down, out hit))
+        {
+            if (hit.distance <= 0.5f)
+            {
+                canBeAffectedByGravity = false;
+                rb.isKinematic = true;
+                rb.useGravity = false;
+                shCollider.enabled = false;
+            }
+            else
+                return;
+
+        }
+    }
+
+    bool setState()
+    {
+        if (setState() == true)
+        {
+            canBeAffectedByGravity = false;
+            rb.isKinematic = true;
+            rb.useGravity = false;
+            shCollider.enabled = false;
+        }
+        else if (setState() == false)
+        {
+            rb.isKinematic = false;
+            rb.useGravity = true;
+            shCollider.enabled = true;
+        }
+
+        return setState();
     }
 
     public void OnBulletClose()
@@ -20,9 +69,14 @@ public class TempoWallBreak : MonoBehaviour, IBulletAffect
 
     public void OnHit(DataWeaponMod mod, Vector3 position, float dammage)
     {
-        renderer.enabled = true;
-        rb.isKinematic = false;
-        rb.AddForce(-position.normalized*100);
+        if (canBeAffectedByGravity)
+        {
+            rb.isKinematic = false;
+            rb.useGravity = true;
+            shCollider.enabled = true;
+            rb.AddExplosionForce(1000, gameObject.transform.position, 100);
+            Invoke("setStatic", 5);
+        }
     }
 
     public void OnHitShotGun(DataWeaponMod mod)
