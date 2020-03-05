6b04b971b7d8ea7d85dddf4dfd967fa8e8c4ffb7
SachaYerle <SachaYerle@users.noreply.github.com>
2020-03-05 15:03:07 +0100

    nik

---
+++ Project/Assets/Scripts/DataModels/DataPostEffect.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "DataPostEffect", menuName = "ScriptableObjects/DataPostEffect")]
+public class DataPostEffect : ScriptableObject
+{
+    [Header("Chroma")]
+    public bool chromaChangeDependentFromTimeScale = true;
+    public float chromaTimeTransition = 1;
+    public float chromaMax = 1;
+    public float chromaMin = 0;
+}
---
+++ Project/Assets/Scripts/Managers/PostprocessManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+[RequireComponent(typeof(PostProcessVolume))]  
+public class PostprocessManager : MonoBehaviour
+{
+
+    PostProcessVolume ppVolume = null;
+
+    [SerializeField]
+    DataPostEffect dataPp = null;
+
+    // --- Chroma
+    ChromaticAberration chromaticAberrationEffect;
+    [HideInInspector]
+    public bool isChroma = false;
+
+    // --- Vignette
+    Vignette vignetteEffect;
+
+    // --- DepthOfField
+    DepthOfField dofEffect;
+
+    public static PostprocessManager Instance { get; private set; }
+    void Awake()
+    {
+        Instance = this;
+    }
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // --- Chroma
+        chromaticAberrationEffect = ScriptableObject.CreateInstance<ChromaticAberration>();
+        chromaticAberrationEffect.enabled.Override(true);
+        chromaticAberrationEffect.intensity.Override(0);
+
+        ppVolume = PostProcessManager.instance.QuickVolume(11, 100f, chromaticAberrationEffect);
+
+        // --- Vignette
+        vignetteEffect = ScriptableObject.CreateInstance<Vignette>();
+        vignetteEffect.enabled.Override(true);
+        vignetteEffect.intensity.Override(0.25f);
+        vignetteEffect.smoothness.Override(1);
+
+        // --- Vignette
+        dofEffect = ScriptableObject.CreateInstance<DepthOfField>();
+        dofEffect.enabled.Override(true);
+
+
+        ppVolume = PostProcessManager.instance.QuickVolume(11, 101f, vignetteEffect);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandlerChroma();
+        HandleDepthOfField();
+    }
+
+    void HandlerChroma()
+    {
+        float valueGoTo = dataPp.chromaMin;
+        if (isChroma) valueGoTo = dataPp.chromaMax;
+        chromaticAberrationEffect.intensity.value = Mathf.MoveTowards(chromaticAberrationEffect.intensity.value, valueGoTo, (dataPp.chromaChangeDependentFromTimeScale ? Time.deltaTime: Time.unscaledDeltaTime) * Mathf.Abs(dataPp.chromaMax - dataPp.chromaMin) / dataPp.chromaTimeTransition);
+    }
+
+    void HandleDepthOfField()
+    {
+
+    }
+
+
+}
