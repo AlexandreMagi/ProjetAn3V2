d5a01f3054b26e492088efc1cded282bc9200ce0
SachaYerle <SachaYerle@users.noreply.github.com>
2020-03-09 15:21:54 +0100

    Game end choice animate start and while

--- Project/Assets/Scripts/Ui/EndGameChoice.cs
+++ Project/Assets/Scripts/Ui/EndGameChoice.cs
@@ -23,12 +23,28 @@ public class EndGameChoice : MonoBehaviour
     [SerializeField] float bumpAnimAmplitude = 0.2f;
     float bumpAnimPurcentage = 1;
 
+    [SerializeField] Color colorBase = Color.white;
+    [SerializeField] Color colorDanger = Color.red;
+    [SerializeField] float dangerTransitionLerpSpeed = 5;
+    [SerializeField] int remainingSecondDanger = 5;
+    [SerializeField] float dangerScale = 1.5f;
+    float baseScaleCoutdown = 1;
+
+    [SerializeField] Animator anmtrDisplay = null;
+
     public void SetupChoice(int publicMalus, int purcentageChance)
     {
         rootGameEnd.SetActive(true);
         publicMalusText.text = "- " + publicMalus;
         publicChanceSurvival.text = purcentageChance + " %";
         countdown.text = Mathf.RoundToInt(Main.Instance.TimeRemainingBeforeGameOver).ToString();
+        anmtrDisplay.SetTrigger("Pop");
+    }
+
+    public void ActivateChoice()
+    {
+        //anmtrDisplay.StopPlayback();
+        anmtrDisplay.enabled = false;
     }
 
     public void EndChoice()
@@ -39,19 +55,30 @@ public class EndGameChoice : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        countdown.text = Mathf.RoundToInt(Main.Instance.TimeRemainingBeforeGameOver).ToString();
+        countdown.text = Mathf.CeilToInt(Main.Instance.TimeRemainingBeforeGameOver).ToString();
         if (countdown.text != lastText)
             bumpAnimPurcentage = 0;
         lastText = countdown.text;
 
+        if (Mathf.CeilToInt(Main.Instance.TimeRemainingBeforeGameOver) <= remainingSecondDanger)
+        {
+            countdown.color = Color.Lerp(countdown.color, colorDanger, Time.unscaledDeltaTime * dangerTransitionLerpSpeed);
+            baseScaleCoutdown = Mathf.Lerp(baseScaleCoutdown, dangerScale, Time.unscaledDeltaTime * dangerTransitionLerpSpeed);
+        }
+        else
+        {
+            countdown.color = colorBase;
+            baseScaleCoutdown = 1;
+        }
+
         if (bumpAnimPurcentage < 1)
         {
-            countdown.transform.localScale = Vector3.one + Vector3.one * bumpAnimCurve.Evaluate(bumpAnimPurcentage) * bumpAnimAmplitude;
+            countdown.transform.localScale = Vector3.one * baseScaleCoutdown + Vector3.one * bumpAnimCurve.Evaluate(bumpAnimPurcentage) * bumpAnimAmplitude;
             bumpAnimPurcentage += Time.unscaledDeltaTime / bumpAnimTime;
             if (bumpAnimPurcentage > 1)
             {
                 bumpAnimPurcentage = 1;
-                countdown.transform.localScale = Vector3.one;
+                countdown.transform.localScale = Vector3.one * baseScaleCoutdown;
             }
         }
     }
