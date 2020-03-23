4f445a5f4985b2efb7cedec02e4e046d6c71ebbb
AlexandreMagi <almagisson@orange.fr>
2020-03-23 13:18:02 +0100

    Titles

--- Project/Assets/Scripts/Managers/TitlesManager.cs
+++ Project/Assets/Scripts/Managers/TitlesManager.cs
@@ -1,15 +1,148 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Xml.Serialization;
 using UnityEngine;
 
 public class TitlesManager : MonoBehaviour
 {
     public static TitlesManager Instance { get; private set; }
 
+    TitlesDatabase dbTitles;
+
+    public struct Title
+    {
+        public string titleName;
+        public string titleDesc;
+        public bool isUnlocked;
+        public int bonusScore;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
         Instance = this;
+        dbTitles = new TitlesDatabase();
+        //SaveTitles();
+        LoadTitles();
+    }
+    
+    void SaveTitles()
+    {
+        //Création du sérializer et du stream de fichier.
+        XmlSerializer serializer = new XmlSerializer(typeof(TitlesDatabase));
+
+        //Sauvegarde try
+        try
+        {
+            FileStream stream = new FileStream(Application.dataPath + "/Leaderboard/titles.xml", FileMode.Create);
+            serializer.Serialize(stream, dbTitles);
+
+            //Fermeture du stream
+            stream.Close();
+        }
+        catch (System.Exception e)
+        {
+            Debug.LogError($"Erreur lors de la sauvegarde ! Error : {e.Message}");
+        }
     }
 
+    void LoadTitles()
+    {
+        //Création du sérializer et du stream de fichier.
+        XmlSerializer serializer = new XmlSerializer(typeof(TitlesDatabase));
+
+        try
+        {
+            FileStream stream = new FileStream(Application.dataPath + "/Leaderboard/titles.xml", FileMode.Open);
+
+            //Lecture
+            dbTitles = serializer.Deserialize(stream) as TitlesDatabase;
+
+            //Fermeture du stream
+            stream.Close();
+        }
+        catch (System.Exception e)
+        {
+            //Si le fichier est pas trouvé en gros, en créer un nouveau
+            Debug.LogWarning($"Aucune donnée de titres n'a été détectée.");
+
+        }
+
+
+    }
+
+    public void ChangeTitleState(string titleName, bool unlocked)
+    {
+        Title titleToChange;
+        bool changeDoable = false;
+        foreach(Title titleInTab in dbTitles.titlesRegistered)
+        {
+            if(titleName == titleInTab.titleName)
+            {
+                titleToChange = titleInTab;
+                changeDoable = true;
+                break;
+            }
+        }
+
+        if(changeDoable)
+        {
+            titleToChange.isUnlocked = unlocked;
+        }
+    }
+
+    public bool GetTitleState(string titleName)
+    {
+        foreach (Title titleInTab in dbTitles.titlesRegistered)
+        {
+            if (titleName == titleInTab.titleName)
+            {
+                return titleInTab.isUnlocked;
+            }
+        }
+
+        return false;
+    }
+
+    public string GetTitleDesc(string titleName)
+    {
+        foreach (Title titleInTab in dbTitles.titlesRegistered)
+        {
+            if (titleName == titleInTab.titleName)
+            {
+                return titleInTab.titleDesc;
+            }
+        }
+
+        return "";
+    }
+
+    public List<Title> GetAllTitles()
+    {
+        return dbTitles.titlesRegistered;
+    }
 }
+
+public class TitlesDatabase
+{
+    public List<TitlesManager.Title> titlesRegistered;
+
+    public TitlesDatabase()
+    {
+        titlesRegistered = new List<TitlesManager.Title>();
+
+        /*
+        TitlesManager.Title init = new TitlesManager.Title
+        {
+            titleName = "Truc",
+            titleDesc = "Desc",
+            isUnlocked = false
+        };
+
+        titlesRegistered.Add(init);
+        */
+    }
+}
+
+
