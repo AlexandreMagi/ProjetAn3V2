1a8b8a1f830c21fa7c6ea037e1ddd57f9da52d4c
SachaYerle <49589025+SachaYerle@users.noreply.github.com>
2020-04-29 14:18:09 +0200

    Meta et display orb tuto

--- Project/Assets/Scripts/Managers/MannequinManager.cs
+++ Project/Assets/Scripts/Managers/MannequinManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class MannequinManager : MonoBehaviour
 {
@@ -21,18 +22,24 @@ public class MannequinManager : MonoBehaviour
     [SerializeField]
     float timeBeforeStart = 0;
 
+    [SerializeField]
+    TextMeshPro display = null;
+
     // Start is called before the first frame update
     void Start()
     {
         mans = new List<Mannequin>(GetComponentsInChildren<Mannequin>());
+        UpdateText();
+
     }
 
 
     public void ChildDied()
     {
         numberOfMannequinsDied++;
+        UpdateText();
 
-        if(numberOfMannequinsDied == mans.Count)
+        if (numberOfMannequinsDied == mans.Count)
         {
             if (nextSequenceOnMansDead)
                 TriggerUtil.TriggerSequence(timeBeforeStart);
@@ -42,4 +49,11 @@ public class MannequinManager : MonoBehaviour
                 TriggerUtil.TriggerAnimators(timeBeforeStart, anims, false);
         }
     }
+
+    void UpdateText()
+    {
+        if (display != null)
+            display.text = numberOfMannequinsDied.ToString() + "/" + mans.Count.ToString();
+    }
+
 }
