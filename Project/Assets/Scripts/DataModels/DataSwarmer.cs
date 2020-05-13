4217e688428a2f4bb0046d546c20b2b7870caa63
AlexandreMagi <almagisson@orange.fr>
2020-05-13 17:30:17 +0200

    Nouveaux déplacements

--- Project/Assets/Scripts/DataModels/DataSwarmer.cs
+++ Project/Assets/Scripts/DataModels/DataSwarmer.cs
@@ -20,7 +20,7 @@ public class DataSwarmer : DataEnemy
 
     public float damage;
 
-    public float upScale;
+    public float accelerationConversionRate = .1f;
 
     public float distanceToTargetEnemy;
 
