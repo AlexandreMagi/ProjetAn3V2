b8af9a1a1170c9117a6588e20acb540eadd36b90
SachaYerle <49589025+SachaYerle@users.noreply.github.com>
2020-05-24 12:45:46 +0200

    Du son

---
+++ Project/Assets/Scripts/Sound/PublicLoopSoundHandler.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PublicLoopSoundHandler : MonoBehaviour
+{
+    public static PublicLoopSoundHandler Instance { get; private set; }
+    void Awake() { Instance = this; }
+
+    AudioSource publicAudioSource = null;
+    float volume = 0;
+    float lastVolume = 0;
+    float aimedVolume = 0;
+    float timeTransition = 1;
+
+    void Start()
+    {
+        publicAudioSource = CustomSoundManager.Instance.PlaySound("SE_LoopCrowdCheer", "PublicAmbiant", CameraHandler.Instance.renderingCam.transform, 0.2f, true);
+        publicAudioSource.volume = 0;
+    }
+
+    void Update()
+    {
+        volume = Mathf.MoveTowards(volume, aimedVolume, Mathf.Abs(volume - lastVolume) * Time.deltaTime / timeTransition);
+    }
+
+    public void ChangePublicVolume (float volumeAimed, float time)
+    {
+        lastVolume = publicAudioSource.volume;
+        aimedVolume = volumeAimed;
+        timeTransition = time;
+    }
+
+}
