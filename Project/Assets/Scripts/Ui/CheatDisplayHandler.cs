aea6e00c456c1103ed20ad71f40a84e640f8a757
SachaYerle <SachaYerle@users.noreply.github.com>
2020-06-05 12:18:01 +0200

    Affichage du cheat et des prenoms à la fin

---
+++ Project/Assets/Scripts/Ui/CheatDisplayHandler.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CheatDisplayHandler : MonoBehaviour
+{
+
+    public static CheatDisplayHandler Instance { get; private set; }
+    void Awake()
+    {
+        Instance = this;
+    }
+
+    [SerializeField] Text hasCheatedText = null;
+    [SerializeField] Text godModText = null;
+
+    bool textsDisplayed = true;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (godModText != null)
+            godModText.text = "";
+        if (hasCheatedText != null)
+            hasCheatedText.text = "";
+    }
+
+    public void GodMod (bool enabled)
+    {
+        if (godModText != null)
+            godModText.text = enabled ? "GodMod Enabled" : "";
+    }
+    public void HasCheated ()
+    {
+        if (hasCheatedText != null)
+            hasCheatedText.text = "Cheated Run";
+    }
+
+    public void ChangeCheatDisplay()
+    {
+        textsDisplayed = !textsDisplayed;
+
+        if (godModText != null)
+            godModText.gameObject.SetActive(textsDisplayed);
+        if (hasCheatedText != null)
+            hasCheatedText.gameObject.SetActive(textsDisplayed);
+
+    }
+
+
+}
