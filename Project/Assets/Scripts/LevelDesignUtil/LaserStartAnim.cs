891bf5c669ea5cecbb2547e47c07f81bd17fc190
AnlaXix <wirtz.maxime1@gmail.com>
2020-06-29 12:43:24 +0200

    Modif et finition LD

---
+++ Project/Assets/Scripts/LevelDesignUtil/LaserStartAnim.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserStartAnim : MonoBehaviour
+{
+
+    [SerializeField]
+    float minTimeBeforeStart = 1f;
+
+    [SerializeField]
+    float maxTimeBeforeStart = 3f;
+
+    Animator laserAnimator = null;
+
+    private void Awake()
+    {
+        laserAnimator = GetComponent<Animator>();
+
+        StartCoroutine(startAnimation());
+    }
+
+    IEnumerator startAnimation()
+    {
+        yield return new WaitForSeconds(Random.Range(minTimeBeforeStart, maxTimeBeforeStart));
+
+        laserAnimator.SetTrigger("MakeAction");
+
+        yield break;
+    }
+}
