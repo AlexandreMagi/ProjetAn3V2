339e492cb28d8ff04b6229719ae127a358f07b3a
SachaYerle <SachaYerle@users.noreply.github.com>
2020-06-29 15:47:43 +0200

    Debug timing

--- Project/Assets/Scripts/LevelDesignUtil/CommentTrigger.cs
+++ Project/Assets/Scripts/LevelDesignUtil/CommentTrigger.cs
@@ -47,50 +47,50 @@ public class CommentTrigger : MonoBehaviour
                 case typeOfComment.entreeFinalBattle:
                     Main.Instance.PlaySoundWithDelay("PresA_Entree_Final_Battle"    , "Comment", Main.Instance.CommentAVolume, delay + 0.0f);
                     Main.Instance.PlaySoundWithDelay("PresB_Entree_Final_Battle"    , "Comment", Main.Instance.CommentBVolume, delay + 2.0f);
-                    SubtitleManager.Instance.SetSubtitle("Our Gladiator has reached the main stage !",0, 4, 0);
-                    SubtitleManager.Instance.SetSubtitle("Now the real fun begins !",1, 3, 2.2f);
+                    SubtitleManager.Instance.SetSubtitle("Our Gladiator has reached the main stage !",0, 4, delay + 0);
+                    SubtitleManager.Instance.SetSubtitle("Now the real fun begins !",1, 3, delay + 2.2f);
                     break;
                 case typeOfComment.introAtterissage:
                     Main.Instance.PlaySoundWithDelay("PresA_Intro_Atterrissage", "Comment", Main.Instance.CommentAVolume, delay + 0.0f);
                     Main.Instance.PlaySoundWithDelay("PresB_Intro_Atterrissage", "Comment", Main.Instance.CommentBVolume, delay + 4.5f);
-                    SubtitleManager.Instance.SetSubtitle("Please give a warm welcome to our new gladiator in DEATH LIVE!",0, 5f, 0);
-                    SubtitleManager.Instance.SetSubtitle("Welcome, Good luck and.. Have fun !",1, 5, 4.8f);
+                    SubtitleManager.Instance.SetSubtitle("Please give a warm welcome to our new gladiator in DEATH LIVE!",0, 5f, delay + 0);
+                    SubtitleManager.Instance.SetSubtitle("Welcome, Good luck and.. Have fun !",1, 5, delay + 4.8f);
                     break;
                 case typeOfComment.introCouloir:
                     Main.Instance.PlaySoundWithDelay("PresA_Intro_Couloirs", "Comment", Main.Instance.CommentAVolume, delay + 0.0f);
                     Main.Instance.PlaySoundWithDelay("PresB_Intro_Couloirs", "Comment", Main.Instance.CommentBVolume, delay + 3.2f);
-                    SubtitleManager.Instance.SetSubtitle("Is this challenger ready to face the dangers of the arena ?",0, 5f, 0);
-                    SubtitleManager.Instance.SetSubtitle("Let's see how they hold up against swarmers !",1, 5, 3.2f);
+                    SubtitleManager.Instance.SetSubtitle("Is this challenger ready to face the dangers of the arena ?",0, 5f, delay + 0);
+                    SubtitleManager.Instance.SetSubtitle("Let's see how they hold up against swarmers !",1, 5, delay + 3.2f);
                     break;
                 case typeOfComment.minigun:
                     Main.Instance.PlaySoundWithDelay("PresA_Minigun", "Comment", Main.Instance.CommentAVolume, delay + 0.0f);
                     Main.Instance.PlaySoundWithDelay("PresB_Minigun", "Comment", Main.Instance.CommentBVolume, delay + 2.3f);
-                    SubtitleManager.Instance.SetSubtitle("Oh now he's packing the big gun !", 0,5f, 0);
-                    SubtitleManager.Instance.SetSubtitle("Let's see some bloood !", 1,3, 2.4f);
+                    SubtitleManager.Instance.SetSubtitle("Oh now he's packing the big gun !", 0,5f, delay + 0);
+                    SubtitleManager.Instance.SetSubtitle("Let's see some bloood !", 1,3, delay + 2.4f);
                     break;
                 case typeOfComment.salleSecrete:
                     Main.Instance.PlaySoundWithDelay("PresA_Salle_Secrete", "Comment", Main.Instance.CommentAVolume, delay + 0.0f);
                     Main.Instance.PlaySoundWithDelay("PresB_Salle_Secrete", "Comment", Main.Instance.CommentBVolume, delay + 0.6f);
-                    SubtitleManager.Instance.SetSubtitle("Amazing ! A secret room!",0, 5f, 0);
-                    SubtitleManager.Instance.SetSubtitle("Jackpot !",1, 1.5f, 1.7f);
+                    SubtitleManager.Instance.SetSubtitle("Amazing ! A secret room!",0, 5f, delay + 0);
+                    SubtitleManager.Instance.SetSubtitle("Jackpot !",1, 1.5f, delay + 1.7f);
                     break;
                 case typeOfComment.tourDeVerre:
                     Main.Instance.PlaySoundWithDelay("PresA_Tour_De_Verre", "Comment", Main.Instance.CommentAVolume, delay + 0.0f);
                     Main.Instance.PlaySoundWithDelay("PresB_Tour_De_Verre", "Comment", Main.Instance.CommentBVolume, delay + 2f);
-                    SubtitleManager.Instance.SetSubtitle("And here comes a new adversary !", 0, 5f, 0);
-                    SubtitleManager.Instance.SetSubtitle("This bad boy will scatter you anytime !", 1, 3.8f, 2.3f);
+                    SubtitleManager.Instance.SetSubtitle("And here comes a new adversary !", 0, 5f, delay + 0);
+                    SubtitleManager.Instance.SetSubtitle("This bad boy will scatter you anytime !", 1, 3.8f, delay + 2.3f);
                     break;
                 case typeOfComment.trap:
                     Main.Instance.PlaySoundWithDelay("PresA_Trap", "Comment", Main.Instance.CommentAVolume, delay + 0.0f);
                     Main.Instance.PlaySoundWithDelay("PresB_Trap", "Comment", Main.Instance.CommentBVolume, delay + 2.2f);
-                    SubtitleManager.Instance.SetSubtitle("Watch out ! This is going to be deadly !", 0, 5f, 0);
-                    SubtitleManager.Instance.SetSubtitle("Come on ! Squish'im like a bug !", 1, 3.3f, 2.6f);
+                    SubtitleManager.Instance.SetSubtitle("Watch out ! This is going to be deadly !", 0, 5f, delay + 0);
+                    SubtitleManager.Instance.SetSubtitle("Come on ! Squish'im like a bug !", 1, 3.3f, delay + 2.6f);
                     break;
                 case typeOfComment.win:
                     Main.Instance.PlaySoundWithDelay("PresA_Win", "Comment", Main.Instance.CommentAVolume, delay + 0.0f);
                     Main.Instance.PlaySoundWithDelay("PresB_Win", "Comment", Main.Instance.CommentBVolume, delay + 3.5f);
-                    SubtitleManager.Instance.SetSubtitle("Dear viewers, Death Live has a NEW CHAMPION!!!", 0, 5f, 0);
-                    SubtitleManager.Instance.SetSubtitle("Now THAT'S what I call sport !", 1, 3f, 3.5f);
+                    SubtitleManager.Instance.SetSubtitle("Dear viewers, Death Live has a NEW CHAMPION!!!", 0, 5f, delay + 0);
+                    SubtitleManager.Instance.SetSubtitle("Now THAT'S what I call sport !", 1, 3f, delay + 3.5f);
                     break;
                 default:
                     break;
